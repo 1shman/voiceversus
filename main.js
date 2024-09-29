9ee8ed7fcb98e09a152c74974120a24e022b543b
khhan <khhan@umich.edu>
2024-09-29 02:08:28 -0400

    Updated Groq LLM prompt to provide better summaries

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const server = app.listen(3000);
 const io = require('socket.io')(server);
 
 const groq = new Groq({ apiKey: process.env.GROQ_SECRET_KEY });
+
+const prevSummaries = new Map();
 var summary;
 
 app.use(express.static(path.join(__dirname, 'views')));
@@ -15,19 +17,26 @@ app.use(express.static(path.join(__dirname, 'views')));
 io.on('connection', (socket) => {
     console.log("connection");
     socket.on('message', async function(text) {
-        await summarizer(text);
+        await summarizer(socket.id, text);
+        prevSummaries.set(socket.id, summary);
         socket.emit('sent-message', true, text, summary);
         socket.broadcast.emit('sent-message', false, text, summary);
     });
 });
 
-async function summarizer(text) {
+async function summarizer(id, text) {
+    var content;
+    if (prevSummaries.has(id)) {
+        content = `Previous summary: '${prevSummaries.get(id)}'. Create bullet points (use a -) of important facts or opinions included in the text. Add onto previous bullet points. Text to be summarized: '${text}'`
+    } else {
+        content = `This person is making an argument in a debate. Create bullet points of important facts or opinions included in their argument. Text to be summarized: '${text}'`
+    }
     const summarizer = await groq.chat.completions
         .create({
             messages: [
                 {
                     role: "user",
-                    content: `Summarize the following text in less than 100 words: ${text}`,
+                    content: content,
                 },
             ],
             model: "mixtral-8x7b-32768",
