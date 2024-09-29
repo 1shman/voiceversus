a593b86357101eb1c8ab74188307a23d594206d7
Ishraq Rahman <ishman@umich.edu>
2024-09-28 21:20:07 -0400

    live share session1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// this is a test
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = app.listen(3000);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    socket.on('transcript', (transcript) => {
+        socket.broadcast.emit('sent-transcript', transcript);
+    });
+});
---
+++ message.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const Groq = require('groq-sdk');
+
+const socket = io();
+const groq = new Groq();
+
+// TODO: Put appropriate elements here
+const messageContainer = document.getElementById('message-container');
+const audioForm = document.getElementById('audio-form');
+const audioPath = document.getElementById('message');
+
+
+audioForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    transcribe();
+});
+
+// Get the transcript from the audio file at audioPath
+// then emit to main.js
+// Add to UI
+async function transcribe() {
+    // const transcript = await groq.audio.transcriptions.create({
+    //     file: fs.createReadStream(audioPath.value),
+    //     model: "distil-whisper-large-v3-en",
+    //     response_format: "text"
+    // });
+    const transcript = audioPath.value;
+    socket.emit('transcript', transcript);
+    addMessageToUI(true, transcript);
+    audioPath.value = "";
+}
+
+// User receives a message
+socket.on('sent-transcript', (transcript) => {
+    addMessageToUI(false, transcript);
+});
+
+// Add message to the UI, e.g., a chat message
+function addMessageToUI(isOwnMessage, transcript) {
+    // add style based on if it is your message or not
+    const element = `
+        <li class="${isOwnMessage ? "messageRight" : "messageLeft"}">
+            <p class="message">
+            ${transcript}
+            </p>
+        </li>
+        `;
+    messageContainer.innerHTML += element;
+    scrollToBottom();
+}
+
+// When a message is put in, users automatically scroll to the bottom
+function scrollToBottom() {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight);
+}
